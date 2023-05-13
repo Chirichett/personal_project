8f98a20d2c3b79f1468c06067401fe94160a27cb
Francesco <92933627+Chirichett@users.noreply.github.com>
2023-05-13 14:17:25 +0200

    Inserimento cartelle e colori

---
+++ lib/models/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Color.fromARGB(255, 34, 34, 34);
+const appColor = Color.fromARGB(255, 0, 140, 255);
+const appShadowColor = Color.fromARGB(125, 0, 140, 255);
+const appDarkColor = Color.fromARGB(255, 0, 100, 182);
+const warningColor = Color.fromARGB(255, 230, 0, 0);
+const attentionColor = Color.fromARGB(255, 230, 176, 0);
+const doneColor = Color.fromARGB(255, 28, 236, 0);
---
+++ lib/widget/first_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
